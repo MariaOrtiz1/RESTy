8d41bc8e6f918cfc3db0e0f634fbd987a7b7440b
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-09-21 13:30:05 -0700

    Test passing!

--- src/containers/RestyContainer.test.jsx
+++ src/containers/RestyContainer.test.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import { rest } from 'msw';
-// import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import RESTyContainer from './RestyContainer';
 
-// const mockServer = setupServer(
-//   rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
-//     return res(
-//       ctx.json(
-//         [
-//           {
-//             id: '5f5fb4bbbfd05c2aed8e474',
-//             name: 'Aurora',
-//             image: 'example.com/image.png',
-//             species: 'penguin',
-//             personality: 'normal',
-//             quote: 'Always keep your cool.',
-//           }
-//         ]
-//       )
-//     );
-//   })
-// );
+const mockServer = setupServer(
+  rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
+    return res(
+      ctx.json(
+        [
+          {
+            id: '5f5fb4bbbfd05c2aed8e474',
+            name: 'Aurora',
+            image: 'example.com/image.png',
+            species: 'penguin',
+            personality: 'normal',
+            quote: 'Always keep your cool.',
+          }
+        ]
+      )
+    );
+  })
+);
 
 describe('RESTycontainer', () => {
-  // beforeAll(() => mockServer.listen());
-  // afterAll(() => mockServer.close());
+  beforeAll(() => mockServer.listen());
+  afterAll(() => mockServer.close());
 
   // eslint-disable-next-line max-len
   it('should display a controls section and a display section', async () => {
@@ -47,9 +47,9 @@ describe('RESTycontainer', () => {
 
     return waitFor(() => {
       // eslint-disable-next-line max-len
-      const response = screen.findByRole('article', { name: 'display-response' });
+      const response = screen.getByRole('article', { name: 'display-response' });
 
       expect(response).toMatchSnapshot();
-    });
+    }, { timeout: 2000 });
   });
 });
